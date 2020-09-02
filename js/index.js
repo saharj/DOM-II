8e4a151bb587b4d7b3f84757c315190add7c937f
Sahar Jafari <shr.jafari@gmail.com>
2020-09-01 21:48:49 -0700

    Add the rest 5

--- js/index.js
+++ js/index.js
@@ -2,16 +2,25 @@
 const scrollHome = (e) => {
   e.target.style.cursor = "all-scroll";
 };
-const homeElem = document.querySelector("body");
-homeElem.addEventListener("wheel", scrollHome);
-homeElem.addEventListener("mousemove", (e) => {
-  e.target.style.cursor = "unset";
+
+const bodyElem = document.querySelector("body");
+
+bodyElem.addEventListener("wheel", scrollHome);
+bodyElem.addEventListener("mousemove", (e) => {
+  // Check if body has an "all-scroll" cursor
+  if (bodyElem.style.cursor === "all-scroll") {
+    e.target.style.cursor = "unset";
+  }
 });
-homeElem.style.cursor = "default";
+bodyElem.addEventListener("keydown", (e) => {
+  if (e.keyCode === 70) {
+    alert("WOOOOOO");
+  }
+});
 
 const header = document.querySelector(".main-navigation");
 header.addEventListener(
-  "mouseover",
+  "mouseenter",
   (e) => {
     header.style.backgroundColor = "#f9dfc9";
   },
@@ -28,13 +37,65 @@ header.addEventListener(
 
 const contentPick = document.querySelector(".content-pick");
 contentPick.addEventListener(
-  "click",
+  "mousedown",
   (e) => {
     e.preventDefault();
     if (e.target.className === "btn") {
-      alert(
+      confirm(
         `You requested a sign up for  "${e.target.parentNode.firstElementChild.innerText}"`
       );
+    }
+  },
+  false
+);
+
+const navItems = document.querySelectorAll(".nav-link");
+navItems.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    const currentColor = e.target.style.color;
+    e.target.style.color = currentColor === "blue" ? null : "blue";
+  });
+
+  item.addEventListener("mouseover", (e) => {
+    e.target.style.cursor = "pointer";
+  });
+});
+
+const pics = document.querySelectorAll("img");
+pics.forEach((image) => {
+  image.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    alert("Sorry! Right click is not allowed on this website.");
+  });
+});
+
+const pageLogo = document.querySelector(".logo-heading");
+pageLogo.addEventListener("mouseover", (e) => {
+  header.appendChild(createTooltip());
+});
+
+const createTooltip = () => {
+  const tooltip = document.createElement("span");
+  tooltip.innerHTML = "Press `f` and see what happens :)";
+  tooltip.className = "tooltip";
+  tooltip.style.fontFamily = "Indie Flower";
+  tooltip.style.fontSize = "1.5rem";
+  tooltip.style.border = "1px solid black";
+  tooltip.style.backgroundColor = "#fff";
+  tooltip.style.position = "absolute";
+  tooltip.style.left = "23%";
+  tooltip.style.top = "7rem";
+  tooltip.style.padding = "1rem";
+  return tooltip;
+};
+
+pageLogo.addEventListener(
+  "mouseleave",
+  (e) => {
+    const tooltip = document.querySelector(".main-navigation span");
+    if (tooltip) {
+      header.removeChild(tooltip);
     }
   },
   false
