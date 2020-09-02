2964e100c5e32a105f5e706f1c663932a6593cf0
Sahar Jafari <shr.jafari@gmail.com>
2020-09-01 18:42:50 -0700

    Add 5 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // Your code goes here
+const scrollHome = (e) => {
+  e.target.style.cursor = "all-scroll";
+};
+const homeElem = document.querySelector("body");
+homeElem.addEventListener("wheel", scrollHome);
+homeElem.addEventListener("mousemove", (e) => {
+  e.target.style.cursor = "unset";
+});
+homeElem.style.cursor = "default";
+
+const header = document.querySelector(".main-navigation");
+header.addEventListener(
+  "mouseover",
+  (e) => {
+    header.style.backgroundColor = "#f9dfc9";
+  },
+  false
+);
+
+header.addEventListener(
+  "mouseleave",
+  (e) => {
+    header.style.backgroundColor = "#fff";
+  },
+  false
+);
+
+const contentPick = document.querySelector(".content-pick");
+contentPick.addEventListener(
+  "click",
+  (e) => {
+    e.preventDefault();
+    if (e.target.className === "btn") {
+      alert(
+        `You requested a sign up for  "${e.target.parentNode.firstElementChild.innerText}"`
+      );
+    }
+  },
+  false
+);
